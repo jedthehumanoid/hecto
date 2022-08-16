43bd9d9bc182a628ad55f4d4051993a28be71849
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2022-08-16 19:37:27 +0200

    exit without panic

--- src/editor.rs
+++ src/editor.rs
@@ -8,12 +8,15 @@ pub struct Editor {
 }
 
 impl Editor {
-    pub fn run(&self) {
+    pub fn run(&mut self) {
         let _stdout = stdout().into_raw_mode().unwrap();
 
         loop {
             if let Err(error) = self.process_keypress() {
                 die(&error);
+            }
+            if self.should_quit {
+                break;
             }
         }
     }
@@ -22,10 +25,10 @@ impl Editor {
         Self { should_quit: false }
     }
 
-    fn process_keypress(&self) -> Result<(), std::io::Error> {
+    fn process_keypress(&mut self) -> Result<(), std::io::Error> {
         let pressed_key = read_key()?;
         match pressed_key {
-            Key::Ctrl('q') => panic!("Program end"),
+            Key::Ctrl('q') => self.should_quit = true,
             _ => (),
         }
         Ok(())
