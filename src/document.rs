6ae2b2ec7c73b74d9f57186c6b9911623a9d3cff
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2022-08-23 21:46:12 +0200

    integer arithmetic 1

--- src/document.rs
+++ src/document.rs
@@ -37,11 +37,15 @@ impl Document {
     }
 
     fn insert_newline(&mut self, at: &Position) {
+        if at.y > self.len() {
+            return;
+        }
         if at.y == self.len() {
             self.rows.push(Row::default());
             return;
         }
         let new_row = self.rows.get_mut(at.y).unwrap().split(at.x);
+        #[allow(clippy::integer_arithmetic)]
         self.rows.insert(at.y + 1, new_row);
     }
 
@@ -64,13 +68,14 @@ impl Document {
         }
     }
 
+    #[allow(clippy::integer_arithmetic)]
     pub fn delete(&mut self, at: &Position) {
         let len = self.len();
         if at.y >= len {
             return;
         }
         self.dirty = true;
-        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y + 1 < len {
             let next_row = self.rows.remove(at.y + 1);
             let row = self.rows.get_mut(at.y).unwrap();
             row.append(&next_row);
