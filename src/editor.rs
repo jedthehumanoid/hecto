3b7b78464abb22bbd3689c262c58dc4b30163d20
Mattias Jadelius <212982+jedthehumanoid@users.noreply.github.com>
2022-08-29 08:43:56 +0200

    highlight matches

--- src/editor.rs
+++ src/editor.rs
@@ -152,6 +152,7 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
+                    editor.document.highlight(Some(query));
                 },
             )
             .unwrap_or(None);
@@ -160,6 +161,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
+        self.document.highlight(None);
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
